8d61dfd144ff2e771613e3a219e133451360f93b
Soma Szoboszlai <somaszoboszlai@gmail.com>
2023-02-04 18:26:16 +0100

    Animate h1

--- src/features/animateTitle.js
+++ src/features/animateTitle.js
@@ -2,12 +2,12 @@ import gsap from 'gsap'
 
 function animateTitle() {
   // Get the element from the DOM
-  const h1Inner = document.querySelector('span.h1-inner')
+  const h1Inner = document.querySelector('.heading-style-h1')
 
   // If it exists, play the aniamtion
   if (h1Inner) {
-    gsap.to(h1Inner, {
-      y: 0,
+    gsap.from(h1Inner, {
+      y: '-100%',
       duration: 0.6,
       ease: 'elastic.out(1, 0.6)',
     })

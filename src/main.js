b1ff72ae47ff92607b4f3ae6f073eb75d0e1d117
Soma Szoboszlai <somaszoboszlai@gmail.com>
2023-02-04 21:44:46 +0100

    Prettier fix

--- src/main.js
+++ src/main.js
@@ -1,10 +1,9 @@
 import './styles/style.css'
-import { gsap } from "gsap";
-import { Flip } from "gsap/Flip";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { gsap } from 'gsap'
+import { Flip } from 'gsap/Flip'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
 
-gsap.registerPlugin(Flip, ScrollTrigger);
-
+gsap.registerPlugin(Flip, ScrollTrigger)
 
 console.log('Soma! It"s working')
 
@@ -17,49 +16,52 @@ console.log('Soma! It"s working')
  * No modifications or updates can be requested.
  * Purchasing this code is not in exchange for any support from Soma Szoboszlai in implementing it in your project or in exchange for any other personal service.
  */
-let tl2 = gsap.timeline({ delay: 0.1 });
-tl2.to(".txt-row", {
-  opacity: 1,
-  ease: "power4.out",
-  duration: 0.3
-}, 0
-);
-gsap.registerPlugin(ScrollTrigger);
+let tl2 = gsap.timeline({ delay: 0.1 })
+tl2.to(
+  '.txt-row',
+  {
+    opacity: 1,
+    ease: 'power4.out',
+    duration: 0.3,
+  },
+  0
+)
+gsap.registerPlugin(ScrollTrigger)
 
-let pixelArr = gsap.utils.toArray("#rect");
-let trends = document.getElementsByClassName("is-trends");
-let five = document.getElementsByClassName("five-pixel");
-let triggerElement = document.getElementsByClassName(".anim-container");
+let pixelArr = gsap.utils.toArray('#rect')
+let trends = document.getElementsByClassName('is-trends')
+let five = document.getElementsByClassName('five-pixel')
+let triggerElement = document.getElementsByClassName('.anim-container')
 
 let tl = gsap.timeline({
   scrollTrigger: {
     //trigger element / viewport
     trigger: triggerElement,
-    start: "top top",
-    end: "center center",
-    scrub: true
-  }
-});
+    start: 'top top',
+    end: 'center center',
+    scrub: true,
+  },
+})
 
 tl.from(trends, {
-  x: "-17em",
-  duration: 4
+  x: '-17em',
+  duration: 4,
 })
   .from(
     five,
     {
       opacity: 1,
-      x: "-10em",
-      duration: 15
+      x: '-10em',
+      duration: 15,
     },
-    ">"
+    '>'
   )
   .from(
     pixelArr,
     {
       opacity: 0,
       stagger: { each: 0.07 },
-      delay: "random(0, 20)"
+      delay: 'random(0, 20)',
     },
-    ">-=30%"
-  );
+    '>-=30%'
+  )

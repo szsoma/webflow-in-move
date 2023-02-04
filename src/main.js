2e5f303e6da7fbe68a76db11f4fbd9b0accb3e96
Soma Szoboszlai <somaszoboszlai@gmail.com>
2023-02-04 20:21:36 +0100

    fix

--- src/main.js
+++ src/main.js
@@ -1,8 +1,3 @@
-import animateTitle from './features/animateTitle'
-import createBadge from './features/createBasge'
 import './styles/style.css'
 
 console.log('Soma! It"s working')
-
-createBadge()
-animateTitle()

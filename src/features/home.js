34a0c6ddd7154d44a3c933587554157ca96d36ad
Soma Szoboszlai <somaszoboszlai@gmail.com>
2023-02-27 15:28:46 +0100

    adding gsap codes home.js

---
+++ src/features/home.js
@@ -0,0 +1,98 @@
+/*!
+ * FluidBlocks v1.0 - https://fluidgo.gumroad.com
+ * @license Copyright 2022, Soma Szoboszlai. All rights reserved.
+ * Purchasing this code gives you the rights to use it in your projects but not to redistribute it to others through cloneables or any other method of sharing the code.
+ * For a license to use this code in a template, please contact Soma Szoboszlai. This code is listed as is.
+ * No modifications or updates can be requested.
+ * Purchasing this code is not in exchange for any support from Soma Szoboszlai in implementing it in your project or in exchange for any other personal service.
+ */
+
+// Site rectangles move down animation
+let rectangleReveal = gsap.utils.toArray('.col4_item')
+
+function rectangleReveal() {
+  let tl = gsap.timeline({
+    scrollTrigger: {
+      //trigger element / viewport
+      trigger: '.section_who',
+      start: 'top center',
+    },
+  })
+  tl.from(rectangleReveal, {
+    y: '0%',
+    stagger: { each: 0.3 },
+    duration: 1.8,
+    ease: 'expo.inOut',
+    delay: 'random(0.1, 0.9)',
+  })
+}
+
+// Project #01 -----------------------------
+
+function project01() {
+  let item1 = $('.wim_project-01-rect1.is-1')
+  let item2 = $('.wim_project-01-ed')
+  let staggerArray = gsap.utils.toArray('.wim_project01-stagger')
+
+  var tl1 = gsap.timeline({
+    defaults: {
+      duration: 1,
+      ease: 'expo.inOut',
+    },
+    repeat: -1,
+    yoyo: true,
+  })
+
+  tl1
+    .to(item1, {
+      scaleY: 4,
+    })
+    .to(item1, {
+      scaleY: 1,
+    })
+    .to(item2, {
+      height: '8%',
+      rotate: 360,
+    })
+    .to(item2, {
+      height: '100%',
+    })
+    .to(
+      '.wim_project-01-rect1.is-2:nth-child(1)',
+      {
+        y: '100%',
+      },
+      '<'
+    )
+    .to('.wim_project-01-rect1.is-2:nth-child(2)', {
+      x: '-100%',
+    })
+    .to('.wim_project-01-rect1.is-2:nth-child(1)', {
+      x: '100%',
+      delay: -1,
+    })
+    .to('.wim_project-01-rect1.is-2:nth-child(2)', {
+      y: '100%',
+    })
+    .to('.wim_project-01-rect1.is-2:nth-child(1)', {
+      y: '0%',
+      delay: -1,
+    })
+    .to('.wim_project-01-rect1.is-2:nth-child(2)', {
+      x: '0%',
+    })
+    .to('.wim_project-01-rect1.is-2:nth-child(1)', {
+      x: '0%',
+      delay: -1,
+    })
+    .to('.wim_project-01-rect1.is-2:nth-child(2)', {
+      y: '0%',
+    })
+    .to('.wim_project-01-circle', {
+      y: '200%',
+      stagger: { each: 0.6 },
+      duration: 1.2,
+      ease: 'expo.inOut',
+      delay: -0.8,
+    })
+}
--- src/main.js
+++ src/main.js
@@ -3,67 +3,13 @@ import { gsap } from 'gsap'
 import { Flip } from 'gsap/Flip'
 import { ScrollTrigger } from 'gsap/ScrollTrigger'
 
-import animateTitle from './features/title-anim'
+// importing my functions
+import rectangleReveal from './features/home.js'
+import project01 from './features/home.js'
 
+// Home javascript section
 gsap.registerPlugin(Flip, ScrollTrigger)
+rectangleReveal()
+project01()
 
-console.log('Soma! It"s working')
-animateTitle()
-/*!
- *Hey Mr. Reverse engineer! Thanks for checking
- * FluidBlocks v1.0 - https://fluidgo.gumroad.com
- * @license Copyright 2022, Soma Szoboszlai. All rights reserved.
- * Purchasing this code gives you the rights to use it in your projects but not to redistribute it to others through cloneables or any other method of sharing the code.
- * For a license to use this code in a template, please contact Soma Szoboszlai. This code is listed as is.
- * No modifications or updates can be requested.
- * Purchasing this code is not in exchange for any support from Soma Szoboszlai in implementing it in your project or in exchange for any other personal service.
- */
-let tl2 = gsap.timeline({ delay: 0.1 })
-tl2.to(
-  '.txt-row',
-  {
-    opacity: 1,
-    ease: 'power4.out',
-    duration: 0.3,
-  },
-  0
-)
-gsap.registerPlugin(ScrollTrigger)
-
-let pixelArr = gsap.utils.toArray('#rect')
-let trends = document.getElementsByClassName('is-trends')
-let five = document.getElementsByClassName('five-pixel')
-let triggerElement = document.getElementsByClassName('.anim-container')
-
-let tl = gsap.timeline({
-  scrollTrigger: {
-    //trigger element / viewport
-    trigger: triggerElement,
-    start: 'top top',
-    end: 'center center',
-    scrub: true,
-  },
-})
-
-tl.from(trends, {
-  x: '-17em',
-  duration: 4,
-})
-  .from(
-    five,
-    {
-      opacity: 1,
-      x: '-10em',
-      duration: 15,
-    },
-    '>'
-  )
-  .from(
-    pixelArr,
-    {
-      opacity: 0,
-      stagger: { each: 0.07 },
-      delay: 'random(0, 20)',
-    },
-    '>-=30%'
-  )
+console.log('It"s working')
